dbf6672da57a7a29b1fdffaefd0c2c467148272f
caiquedegaspari <caique.degaspare@gmail.com>
2022-11-14 21:06:22 -0300

    test: ensure signUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -15,4 +15,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toEqual(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+  it('Should return 400 if no email is provided', () => {
+    // sut = system under test
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any name',
+        password: '123456',
+        passwordConfirmation: '123456'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toEqual(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
