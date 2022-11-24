baf5525d093664ed65138ea36382f08937ee92f7
caiquedegaspari <caique.degaspare@gmail.com>
2022-11-24 19:29:20 -0300

    feat: ensure EmailAdapter returns false if validator returns false

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'jest-environment-node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleDirectories: ['node_modules', 'src']
+}
