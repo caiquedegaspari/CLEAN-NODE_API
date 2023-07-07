7b6a974b4ee9d2c999330371ea91eea2035be22a
caiquedegaspari <caique.degaspare@gmail.com>
2023-07-07 08:36:04 -0300

    refactor: move bcrypt-adapter files to a folder

--- src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt'
-import { Hasher } from '../../data/protocols/criptography/hasher'
+import { Hasher } from '../../../data/protocols/criptography/hasher'
 import { HashComparer } from 'data/protocols/criptography/hash-comparer'
 
 export class BcryptAdapter implements Hasher, HashComparer {
