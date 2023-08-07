1111d770ab4625a1208ee6bd19b2e8d90ddeef16
caiquedegaspari <caique.degaspare@gmail.com>
2023-08-07 20:11:39 -0300

    test: ensure SaveSurveyResultController calls LoadSurveyById with correct values

---
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -0,0 +1,69 @@
+import { SaveSurveyResultController } from './save-survey-result-controller'
+// import { forbidden } from '@/presentation/helpers'
+// import { UnauthorizedError } from '@/presentation/errors'
+import { HttpRequest, SurveyResultModel, SurveyModel, LoadSurveyById } from './save-survey-result-controller-protocols'
+
+type SutTypes = {
+  sut: SaveSurveyResultController
+  loadSurveyByIdStub: LoadSurveyById
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const makeFakeSurveyResult = (): SurveyResultModel => ({
+  id: 'any_id',
+  accountId: 'any_account_id',
+  answer: 'any_answer',
+  date: new Date(),
+  surveyId: 'any_survey_id'
+})
+
+const makeFakeSurvey = (): SurveyModel => ({
+  id: 'any_id',
+  date: new Date(),
+  answers: [
+    { answer: 'any_answer' }
+  ],
+  question: 'any_question'
+})
+
+const makeFakeRequest = (): HttpRequest => ({
+  params: {
+    surveyId: 'any_id'
+  }
+})
+
+const makeLoadSurveyById = (): LoadSurveyById => {
+  class LoadSurveyByIdStub implements LoadSurveyById {
+    async loadById (id: string): Promise<SurveyModel | null> {
+      return await Promise.resolve(makeFakeSurvey())
+    }
+  }
+  return new LoadSurveyByIdStub()
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdStub = makeLoadSurveyById()
+
+  const sut = new SaveSurveyResultController(loadSurveyByIdStub)
+  return {
+    sut,
+    loadSurveyByIdStub
+  }
+}
+
+describe('SaveSurveyResult Controller', () => {
+  it('Should call LoadSurveyById with correct values', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
+    const request = makeFakeRequest()
+    await sut.handle(request)
+    expect(loadByIdSpy).toHaveBeenCalledWith(request.params.surveyId)
+  })
+
+  // it('Should return 403 if LoadSurveyById returns null', async () => {
+  //  const { sut, loadSurveyByIdStub } = makeSut()
+  //  jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(Promise.resolve(null))
+  //  const res = await sut.handle(makeFakeRequest())
+  //  expect(res).toEqual(forbidden(new UnauthorizedError()))
+  // })
+})
