81e0fb7675fb830516238b1207b5832bb2b3a64a
Caique Degaspari <caique.degaspare@gmail.com>
2023-08-19 20:48:00 -0300

    refactor: rename makeFakeServerError to mockServerError

--- src/main/decorators/log-controller-decorator.spec.ts
+++ src/main/decorators/log-controller-decorator.spec.ts
@@ -24,7 +24,7 @@ const mockFakeRequest = (): HttpRequest => ({
   }
 })
 
-const makeFakeServerError = (): HttpResponse => {
+const mockServerError = (): HttpResponse => {
   const fakeError = new Error()
   fakeError.stack = 'any_stack'
   return serverError(fakeError)
@@ -64,7 +64,7 @@ describe('Log Controller Decorator', () => {
   it('Should call LogErrorRepository with correct error if controller returns server error', async () => {
     const { sut, controllerStub, logErrorRepositoryStub } = makeSut()
     const logSpy = jest.spyOn(logErrorRepositoryStub, 'logError')
-    jest.spyOn(controllerStub, 'handle').mockReturnValueOnce(Promise.resolve(makeFakeServerError()))
+    jest.spyOn(controllerStub, 'handle').mockReturnValueOnce(Promise.resolve(mockServerError()))
     await sut.handle(mockFakeRequest())
     expect(logSpy).toHaveBeenCalledWith('any_stack')
   })
