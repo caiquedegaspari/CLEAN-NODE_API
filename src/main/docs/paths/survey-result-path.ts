29769dba124d2d6618026845c7e9a06ef24d605b
Caique Degaspari <caique.degaspare@gmail.com>
2023-08-20 11:50:55 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -43,5 +43,42 @@ export const surveyResultPath = {
         $ref: '#/components/notFound'
       }
     }
+  },
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
+    description: 'Essa rota só pode ser executada po **usuários autenticados**',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      }
+    }
   }
 }
