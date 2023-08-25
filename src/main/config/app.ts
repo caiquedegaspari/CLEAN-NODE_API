056cc926320a3f17678eba20487c10d66157a50e
Caique Degaspari <caique.degaspare@gmail.com>
2023-08-25 07:08:14 -0300

    feat: start login resolver

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,13 @@
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
 import express from 'express'
-import setupSwagger from './config-swagger'
+import setupSwagger from './swagger'
 import setupStaticFiles from './static-files'
+import setupApolloServer from './apollo-server'
 
 const app = express()
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+setupApolloServer(app)
 setupStaticFiles(app)
 setupSwagger(app)
 setupMiddlewares(app)
